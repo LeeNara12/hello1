a6642382e25ee3163af0d4cf5e8f04bc4a092a6f
이나라 <lnr2357@naver.com>
2023-03-15 12:01:25 +0900

    최초커밋

---
+++ src/main/java/com/spring/ex01/Myboot023Application.java
@@ -0,0 +1,13 @@
+package com.spring.ex01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myboot023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myboot023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/ex01/Myboot023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.ex01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myboot023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
